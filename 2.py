20e4f30192200dc810f5d7a7516ad026f03b5c62
ese-ml <dumpertest01@gmail.com>
2025-12-19 02:35:00 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,6 +3,13 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyAfrdlhsrN8DbKj68R7FNcsxqi15PKbA")
 
 model = genai.GenerativeModel("gemini-2.5-flash")
-response = model.generate_content("Write a haiku about artificial intelligence")
 
-print(response.text)
+response = model.generate_content("""
+Write python code
+return ONLY valid Python code.
+No markdown.
+No explanation.""")
+
+code = response.text.strip().replace("```python", "").replace("```", "")
+
+print(code)
